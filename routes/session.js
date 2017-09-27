64e7899c11a62bc80e1052932c777a580c2357be
Togi Dashnyam <togi.berlin@gmail.com>
2017-09-27 17:07:37 +0200

    Added Session routes

---
+++ routes/session.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/newSession', function(req, res) {
+  var newSession = new Session();
+
+  newSession.save(function(err, data) {
+    if (err) {
+      console.log(err);
+      res.render('error');
+    } else {
+      res.redirect('/session/' + data._id);
+    }
+  })
+});
+
+router.get('/session/:id', function(req, res) {
+  if (req.params.id) {
+    Session.findone({ _id: req.params.id }, function(err, data) {
+      if (err) {
+        console.log(err);
+        res.render('error');
+      }
+
+      if (data) {
+        res.render('session', { data: data });
+      } else {
+        res.render('error');
+      }
+    })
+  } else {
+    res.render('error');
+  }
+});
+
+module.exports = router;
