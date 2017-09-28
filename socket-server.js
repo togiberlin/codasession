db487ec4a7de409de96c876f7ec602b797983c90
Togi Dashnyam <togi.berlin@gmail.com>
2017-09-28 15:20:38 +0200

    Added chatbox boilerplate code

---
+++ socket-server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var socketIO = require('socket.io');
+
+module.exports = function(server) {
+  var io = socketIO(server);
+  io.on('connection', function(socket){
+    socket.on('chatMessage ', function(data) {
+      io.emit('chatMessage', data);
+    })
+  })
+}
