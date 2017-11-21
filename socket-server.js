00b134c00d6f638bb671f09849cfe39e42cf590b
Togi Dashnyam <togi.berlin@gmail.com>
2017-11-21 19:41:46 +0100

    Code editor websocket works now

--- socket-server.js
+++ socket-server.js
@@ -1,11 +1,25 @@
 'use strict';
 
 var socketIO = require('socket.io');
+var ot = require('ot');
+var roomList = {};
 
 module.exports = function(server) {
+  var str = '/** Hello there! You can start to type your code here. For example: */ \n\n' +
+            'var greeting = "Hello world!";';
+
   var io = socketIO(server);
   io.on('connection', function(socket) {
     socket.on('joinRoom', function(data) {
+      if (!roomList[data.room]) {
+        var socketIOServer = new ot.EditorSocketIOServer(str, [], data.room, function(socket, cb) {
+          cb(true);
+        });
+        roomList[data.room] = socketIOServer;
+      }
+      roomList[data.room].addClient(socket);
+      roomList[data.room].setName(socket, data.username);
+
       socket.room = data.room;
       socket.join(data.room);
     });
